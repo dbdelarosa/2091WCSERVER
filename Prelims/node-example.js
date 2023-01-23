5bd9b639395101d6caba68091af73ed1841d18f9
dbdelarosa <113003751+dbdelarosa@users.noreply.github.com>
2023-01-23 11:45:23 +0800

    Add files via upload

---
+++ Prelims/node-example.js
@@ -0,0 +1,15 @@
+//Dennis dela Rosa
+//Module 3
+//Class 2 amd 3 Basics of node js
+
+//firstProgram
+
+
+console.log("Welcome to WCserver");
+function Display(x){
+    console.log(x)
+
+}
+Display(200);
+
+var 
