15969793dfc56e5e0c84cb02151a9a6887f5cea9
dbdelarosa <113003751+dbdelarosa@users.noreply.github.com>
2023-01-30 17:04:53 +0800

    1 file modified

--- Prelims/PrelimsHandsOnquiz/app.js
+++ Prelims/PrelimsHandsOnquiz/app.js
@@ -3,10 +3,11 @@ var calc = require('./calculations');
 var hours = 4;
 var rate = 300;
 var days = 6;
-var gross = hours * rate * days;
+
 var tax = gross * 0.1;
 var sss = 1200;
 var pag = 300;
 var phil = 400;
 var totaldeduction = sss + pag + phil + tax;
 var net = gross - totaldeduction;
+var gross = hours * rate * days;
