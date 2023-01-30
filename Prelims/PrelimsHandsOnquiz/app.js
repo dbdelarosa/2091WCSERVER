4c3f41244f32770d55285eed96ba290adeae2663
dbdelarosa <113003751+dbdelarosa@users.noreply.github.com>
2023-01-30 17:05:50 +0800

    1 file modified

--- Prelims/PrelimsHandsOnquiz/app.js
+++ Prelims/PrelimsHandsOnquiz/app.js
@@ -4,10 +4,10 @@ var hours = 4;
 var rate = 300;
 var days = 6;
 
-var tax = gross * 0.1;
 var sss = 1200;
 var pag = 300;
 var phil = 400;
 var totaldeduction = sss + pag + phil + tax;
 var net = gross - totaldeduction;
 var gross = hours * rate * days;
+var tax = gross * 0.1;
