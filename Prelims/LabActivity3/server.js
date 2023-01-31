7b5780b6275d519156389e21625eb50308dcd4ea
dbdelarosa <113003751+dbdelarosa@users.noreply.github.com>
2023-01-30 23:08:43 -0800

    1 file modified, 1 added

--- Prelims/LabActivity3/server.js
+++ Prelims/LabActivity3/server.js
@@ -1,10 +1,10 @@
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
-if (req.url == '/') {
+if (req.url == '/root') {
 res.writeHead(200, { 'Content-Type': 'text/html' });
 res.write(
-'<html><h1>This is Home Page.</h1><body><p>Hello! My name is dennis Dela Rosa of WD - 201/p></body></html>'
+'<html><h1>This is Home Page.</h1><body><p>Hello! My name is dennis Dela Rosa of WD - 201</p></body></html>'
 );
 res.end();
 } else if (req.url == '/student') {
@@ -18,6 +18,24 @@ res.writeHead(200, { 'Content-Type': 'text/html' });
 res.write(
 '<html><h1>This is Admin Page.</h1><body><p>Hello! My name is dennis Dela Rosa of WD - 201</p></body></html>'
 );
+
+} else if (req.url == '/about') {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(
+  '<html><h1>This is Admin Page.</h1><body><p>This Activity will teach on how to deal with simple server and local modules in Node.js</p></body></html>'
+  );
+
+} else if (req.url == '/contact') {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(
+  '<html><h1>This is Contact  Page.</h1><body><p>Dennis dela Rosa, if you want additional details about this activity go to this site http://www.tutorialsteacher.com n</p></body></html>'
+  );
+
+} else if (req.url == '/gallery') {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(
+  '<html><h1>This is Gallery Page.</h1><body><p></p></body></html>'
+  );
 res.end();
 } else res.end('Invalid Request!');
 });
