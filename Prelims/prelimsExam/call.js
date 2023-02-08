f82b5f2f688578e98e1ac8e1e38b5324c93bbf28
dbdelarosa <113003751+dbdelarosa@users.noreply.github.com>
2023-02-08 16:05:34 +0800

    2 files added

---
+++ Prelims/prelimsExam/call.js
@@ -0,0 +1,5 @@
+var {greet,congrats, gbye } = require('./greet.js');
+
+console.log(greet('dennis dela Rosa'));
+console.log( congrats('dennis dela Rosa'));
+console.log( gbye('dennis dela Rosa'));
---
+++ Prelims/prelimsExam/greet.js
@@ -0,0 +1,12 @@
+function greet(name) {
+  return 'Hello' + name;
+}
+
+function congrats(name) {
+  return 'Congratulations' + name;
+}
+
+function gbye(name) {
+  return 'GoodBye' + name;
+}
+module.exports = { greet, congrats, gbye };
