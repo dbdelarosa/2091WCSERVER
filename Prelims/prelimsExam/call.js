9eb9ab31ed970d4be6d5abe5140a0960de8521d9
dbdelarosa <113003751+dbdelarosa@users.noreply.github.com>
2023-02-13 11:21:48 +0800

    1 file modified

--- Prelims/prelimsExam/call.js
+++ Prelims/prelimsExam/call.js
@@ -1,5 +1,5 @@
-var {greet,congrats, gbye } = require('./greet.js');
+var { greet, congrats, gbye } = require('./greet.js');
 
 console.log(greet('dennis dela Rosa'));
-console.log( congrats('dennis dela Rosa'));
-console.log( gbye('dennis dela Rosa'));
+console.log(congrats('dennis dela Rosa'));
+console.log(gbye('dennis dela Rosa'));
